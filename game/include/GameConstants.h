6a102b5b01038cccb114214f2da9d71679dce942
n2omatt <n2o.matt@gmail.com>
2016-06-22 09:03:36 -0300

    - New constants for sounds.

--- game/include/GameConstants.h
+++ game/include/GameConstants.h
@@ -1,6 +1,12 @@
 #ifndef __Game_Frogger_include_GameConstants_h__
 #define __Game_Frogger_include_GameConstants_h__
 
+//Game_Frogger
+#include "GameFrogger_Utils.h"
+
+
+NS_GAME_FROGGER_BEGIN
+
 ////////////////////////////////////////////////////////////////////////////////
 // Fonts                                                                      //
 ////////////////////////////////////////////////////////////////////////////////
@@ -13,4 +19,17 @@ constexpr int kFontSize_Hud_Small =  8;
 //Game
 constexpr int kFontSize_StateText = 40;
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Sounds                                                                     //
+////////////////////////////////////////////////////////////////////////////////
+constexpr auto kSoundName_MusicIntro   = "Music.wav";
+constexpr auto kSoundName_Jump         = "Jump.wav";
+constexpr auto kSoundName_Victory      = "Completion.wav";
+constexpr auto kSoundName_ReachTarget  = "ReachTarget.wav";
+constexpr auto kSoundName_DefeatWater  = "Dead_Water.wav";
+constexpr auto kSoundName_DefeatNormal = "Dead_Normal.wav";
+constexpr auto kSoundName_TimeUp       = "TimeUp.wav";
+
+NS_GAME_FROGGER_END
 #endif // defined(__Game_Frogger_include_GameConstants_h__)
