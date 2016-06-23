ec505116aa753b31347da40d45dfe9ce69dcc030
n2omatt <n2o.matt@gmail.com>
2016-06-22 22:49:07 -0300

    - New constants. - Fix the path for sounds.

--- game/include/GameConstants.h
+++ game/include/GameConstants.h
@@ -10,26 +10,33 @@ NS_GAME_FROGGER_BEGIN
 ////////////////////////////////////////////////////////////////////////////////
 // Fonts                                                                      //
 ////////////////////////////////////////////////////////////////////////////////
-constexpr auto kFontName = "nokiafc22.ttf";
+constexpr auto kFontName         = "Fonts/nokiafc22.ttf";
+constexpr auto kFontName_Credits = "Fonts/SourceCodePro-Regular.ttf";
+
+//Splash
+constexpr int kFontSize_AmazingCowLogoText = 30;
 //Menu
-constexpr int kFontSize_Menu = 22;
+constexpr int kFontSize_Menu            = 22;
+constexpr int kFontSize_AmazingCowText  = 20;
 //Hud
 constexpr int kFontSize_Hud_Big   = 12;
 constexpr int kFontSize_Hud_Small =  8;
 //Game
 constexpr int kFontSize_StateText = 40;
-
+//Credits
+constexpr int kFontSize_CreditsMsg = 18;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Sounds                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
-constexpr auto kSoundName_MusicIntro   = "Music.wav";
-constexpr auto kSoundName_Jump         = "Jump.wav";
-constexpr auto kSoundName_Victory      = "Completion.wav";
-constexpr auto kSoundName_ReachTarget  = "ReachTarget.wav";
-constexpr auto kSoundName_DefeatWater  = "Dead_Water.wav";
-constexpr auto kSoundName_DefeatNormal = "Dead_Normal.wav";
-constexpr auto kSoundName_TimeUp       = "TimeUp.wav";
+constexpr auto kSoundName_AmazingIntro = "Sounds/amazing_intro.wav";
+constexpr auto kSoundName_MusicIntro   = "Sounds/Music.wav";
+constexpr auto kSoundName_Jump         = "Sounds/Jump.wav";
+constexpr auto kSoundName_Victory      = "Sounds/Completion.wav";
+constexpr auto kSoundName_ReachTarget  = "Sounds/ReachTarget.wav";
+constexpr auto kSoundName_DefeatWater  = "Sounds/Dead_Water.wav";
+constexpr auto kSoundName_DefeatNormal = "Sounds/Dead_Normal.wav";
+constexpr auto kSoundName_TimeUp       = "Sounds/TimeUp.wav";
 
 NS_GAME_FROGGER_END
 #endif // defined(__Game_Frogger_include_GameConstants_h__)
