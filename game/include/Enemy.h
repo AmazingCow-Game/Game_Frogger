842019acd77a48604933fdcc40cdaccfa8064c54
n2omatt <n2o.matt@gmail.com>
2016-06-23 19:53:44 -0300

    - Refactor a lot of stuff into the Enemy class. - Make the enemies wrap the screen.

--- game/include/Enemy.h
+++ game/include/Enemy.h
@@ -14,24 +14,53 @@ public:
     Enemy();
     virtual ~Enemy();
 
+    // Update / Draw
+public:
+    virtual void update(float dt) override;
+    virtual void draw() override;
+
 
     // Public Methods //
 public:
+    //Movement Bounds
     void setMovementBounds(int minX, int maxX);
-    int getMovementMinX() const;
-    int getMovementMaxX() const;
+    int  getMovementMinX() const;
+    int  getMovementMaxX() const;
+
+    //Position
+    virtual void setPosition(const Lore::Vector2 &pos) override;
+    virtual const Lore::Vector2& getPosition() const override;
 
     //Bounding Box
-    virtual Lore::Rectangle getBoundingBox() override;
+    virtual Lore::Rectangle getRealBoundingBox    ();
+    virtual Lore::Rectangle getWrappingBoundingBox();
 
     //Collision
     virtual bool checkCollision(const Lore::Rectangle &testRect,
                                 float safeOffset);
 
+
+    // Protected Methods //
+protected:
+    void setWrapping(bool wrapping);
+    bool isWrapping() const;
+
+    const Lore::Vector2 getRealPosition() const;
+    void setRealPosition(const Lore::Vector2 &position);
+
+    const Lore::Vector2 getWrappingPosition() const;
+    void setWrappingPosition(const Lore::Vector2 &position);
+
+
     // iVars //
 private:
-    int m_minX;
-    int m_maxX;
+    int  m_minX;
+    int  m_maxX;
+
+    Lore::Vector2 m_realPosition;
+    Lore::Vector2 m_wrappingPosition;
+
+    bool m_isWrapping;
 };
 
 NS_GAME_FROGGER_END
--- game/src/Enemy.cpp
+++ game/src/Enemy.cpp
@@ -8,7 +8,12 @@ USING_NS_GAME_FROGGER;
 ////////////////////////////////////////////////////////////////////////////////
 // CTOR / DTOR                                                                //
 ////////////////////////////////////////////////////////////////////////////////
-Enemy::Enemy()
+Enemy::Enemy() :
+    m_minX(0),
+    m_maxX(0),
+    m_realPosition    (Lore::Vector2::Zero()),
+    m_wrappingPosition(Lore::Vector2::Zero()),
+    m_isWrapping(false)
 {
     //Empty...
 }
@@ -20,13 +25,85 @@ Enemy::~Enemy()
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// Public Methods                                                             //
+// Update / Draw                                                              //
 ////////////////////////////////////////////////////////////////////////////////
-void Enemy::setMovementBounds(int minX, int maxX)
+void Enemy::update(float dt)
 {
+    auto speed = getSpeed();
     auto width = getSprite().getBounds().getWidth();
 
-    m_minX = minX - width;
+    auto newPos =  m_realPosition + (speed * dt);
+
+    auto moveMin = getMovementMinX();
+    auto moveMax = getMovementMaxX();
+
+    //Assume the we're not wrapping.
+    setWrapping(false);
+
+    //Moving to left...
+    if(newPos.x < moveMin && speed.x < 0)
+    {
+        setWrapping(true);
+        m_wrappingPosition = Lore::Vector2(
+                                 moveMax - (-newPos.x),
+                                 m_realPosition.y
+                             );
+
+        //Already moved too far - It should
+        //be positioned into the "wrapping" position now.
+        if(m_wrappingPosition.x + width < moveMax)
+        {
+            newPos = m_wrappingPosition;
+            setWrapping(false);
+        }
+    }
+
+    //Moving to right...
+    else if(newPos.x + width > moveMax && speed.x > 0)
+    {
+        setWrapping(true);
+        m_wrappingPosition = Lore::Vector2(
+                                 newPos.x - moveMax,
+                                 m_realPosition.y
+                             );
+
+        //Already moved too far - It should
+        //be positioned into the "wrapping" position now.
+        if(m_wrappingPosition.x > moveMin)
+        {
+            newPos = m_wrappingPosition;
+            setWrapping(false);
+        }
+    }
+
+    m_realPosition = newPos;
+}
+
+void Enemy::draw()
+{
+    //Draw the normal sprite.
+    auto sprite = getSprite();
+    sprite.setPosition(getRealPosition());
+    sprite.setColor(Lore::Color::White());
+    sprite.draw();
+
+    if(!isWrapping())
+        return;
+
+    //Draw the wrapping sprite.
+    sprite.setPosition(getWrappingPosition());
+    sprite.setColor(Lore::Color(255, 0, 255));
+    sprite.draw();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Public Methods                                                             //
+////////////////////////////////////////////////////////////////////////////////
+// Movement Bounds
+void Enemy::setMovementBounds(int minX, int maxX)
+{
+    m_minX = minX;
     m_maxX = maxX;
 }
 
@@ -40,31 +117,95 @@ int Enemy::getMovementMaxX() const
     return m_maxX;
 }
 
-//Bounding Box
-Lore::Rectangle Enemy::getBoundingBox()
+
+//Position
+void Enemy::setPosition(const Lore::Vector2 &pos)
 {
-    return getSprite().getBounds();
+    m_realPosition = pos;
 }
+
+const Lore::Vector2& Enemy::getPosition() const
+{
+    return m_realPosition;
+}
+
+
+//Bounding Box
+Lore::Rectangle Enemy::getRealBoundingBox()
+{
+    return Lore::Rectangle(getRealPosition(),
+                           getSprite().getBounds().getSize());
+}
+
+Lore::Rectangle Enemy::getWrappingBoundingBox()
+{
+    if(!isWrapping())
+        return Lore::Rectangle::Empty();
+
+    return Lore::Rectangle(getWrappingPosition(),
+                           getSprite().getBounds().getSize());
+}
+
 
 
 //Collision
-bool Enemy::checkCollision(const Lore::Rectangle &testRect, float safeOffset)
+bool Enemy::checkCollision(const Lore::Rectangle &testRect,
+                           float safeOffset)
 {
-    Lore::Rectangle outRect;
-    bool collided = (getBoundingBox().intersectionRect(testRect, outRect))
-                     && (outRect.getWidth() > safeOffset);
 
-    //Just to add a breakpoint...
-    COREGAME_ONLY_IN_DEBUG({
-        if(collided)
-        {
-             FROGGER_DLOG(
-                "Enemy collided offset %2.f - safe offset %.2f",
-                outRect.getWidth(), safeOffset
-            );
-        }
-    });
+    Lore::Rectangle outRect; //To check against the safe offset.
 
-    return collided;
+    //Check for normal collision.
+    auto normalRect     = getRealBoundingBox();
+    bool collidedNormal = (normalRect.intersectionRect(testRect, outRect))
+                           && (outRect.getWidth() > safeOffset);
+
+
+    //Check wrapping collision.
+    if(isWrapping())
+    {
+        auto wrappingRect     = getWrappingBoundingBox();
+        bool collidedWrapping = (wrappingRect.intersectionRect(testRect, outRect))
+                                 && (outRect.getWidth() > safeOffset);
+
+        return (collidedNormal || collidedWrapping);
+    }
+
+    return collidedNormal;
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Protected Methods                                                          //
+////////////////////////////////////////////////////////////////////////////////
+void Enemy::setWrapping(bool wrapping)
+{
+    m_isWrapping = wrapping;
+}
+
+bool Enemy::isWrapping() const
+{
+    return m_isWrapping;
+}
+
+
+const Lore::Vector2 Enemy::getRealPosition() const
+{
+    return m_realPosition;
+}
+
+void Enemy::setRealPosition(const Lore::Vector2 &position)
+{
+    m_realPosition = position;
+}
+
+
+const Lore::Vector2 Enemy::getWrappingPosition() const
+{
+    return m_wrappingPosition;
+}
+
+void Enemy::setWrappingPosition(const Lore::Vector2 &position)
+{
+    m_wrappingPosition = position;
+}
